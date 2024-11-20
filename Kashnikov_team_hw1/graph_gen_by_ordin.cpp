2c12f8f942ec39f1639e695bcd4edf85216015e2
Ord1nI <ordinandrej@gmail.com>
2024-11-20 19:52:32 +0300

    add graph generation

---
+++ Kashnikov_team_hw1/graph_gen_by_ordin.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+vector<vector<int> > generateBipartiteGraphMatrix(int n1, int n2) {
+    vector<vector<int> > adjacencyMatrix(n1 + n2, vector<int>(n1 + n2, 0));
+    srand(time(0));
+
+    for (int u = 0; u < n1; ++u) {
+        for (int v = 0; v < n2; ++v) {
+            double prob = (double)(rand()) / RAND_MAX;
+            if (prob < 0.5) {
+                adjacencyMatrix[u][n1 + v] = 1;
+                adjacencyMatrix[n1 + v][u] = 1;
+            }
+        }
+    }
+
+    return adjacencyMatrix;
+}
