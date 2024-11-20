af5aacd4cf597c6823ccf06ed9e48370a7cd0d7e
Ord1nI <ordinandrej@gmail.com>
2024-11-20 20:33:03 +0300

    add header file

--- Kashnikov_team_hw1/graph_gen_by_ordin.cpp
+++ Kashnikov_team_hw1/graph_gen_by_ordin.cpp
@@ -1,3 +1,4 @@
+#include "graph_gen_by_ordin.h"
 #include <iostream>
 #include <vector>
 #include <cstdlib>
@@ -12,7 +13,7 @@ vector<vector<int> > generateBipartiteGraphMatrix(int n1, int n2) {
     for (int u = 0; u < n1; ++u) {
         for (int v = 0; v < n2; ++v) {
             double prob = (double)(rand()) / RAND_MAX;
-            if (prob < 0.5) {
+            if (prob < PROBABILITY) {
                 adjacencyMatrix[u][n1 + v] = 1;
                 adjacencyMatrix[n1 + v][u] = 1;
             }
---
+++ Kashnikov_team_hw1/graph_gen_by_ordin.h
@@ -0,0 +1,10 @@
+#ifndef _GRAPH_GEN_BY_ORDIN_
+#define _GRAPH_GEN_BY_ORDIN_
+
+#include <vector>
+
+#define PROBABILITY 0.5
+
+std::vector<std::vector<int> > generateBipartiteGraphMatrix(int n1, int n2);
+
+#endif // _GRAPH_GEN_BY_ORDIN_
