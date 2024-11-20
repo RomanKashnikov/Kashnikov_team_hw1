28e23c649ec706b572b5b3066d7c3a67375a86fd
RomanKashnikov <kashnikov05@list.ru>
2024-11-20 18:56:20 +0200

    test

--- Kashnikov_team_hw1/start.cpp
+++ Kashnikov_team_hw1/start.cpp
@@ -2,7 +2,44 @@
 
 using namespace std;
 
+
+template <typename T>
+T input_validation(const string message, const int min, const int max) {
+    T value;
+
+    cout << message;
+    while ((cin >> value).fail()
+        || cin.peek() != '\n'
+        || value < min || value > max)
+    {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cout << "Try another value:  ";
+    }
+    cin.clear();
+    cin.ignore(10000, '\n');
+    cerr << value << endl;
+    return value;
+}
+
+void menu() {
+    cout << "Menu" << endl << endl;
+    cout << "1 - Generate bipartite graph" << endl << endl;
+    cout << "Methods for finding the number of matchings:" << endl;
+    cout << "2 - " << endl;
+    cout << "3 - " << endl;
+    cout << "4 - " << endl;
+    cout << "5 - " << endl;
+    cout << "0 - Exit" << endl << endl;
+}
+
+
 int main()
 {
-	cout << "xyпирлирлирлирлилz";
+    while (1) {
+        menu();
+
+        int option;
+        option = input_validation<int>("Choose the option:  ", 0, 7);
+    }
 }
