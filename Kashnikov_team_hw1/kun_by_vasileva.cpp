febcebfa77807f9243bf64eb927534a522cfcb98
MariaVasileva04 <masha0042912@gmail.com>
2024-11-24 12:15:42 +0300

    алгоритм Куна для заданной вручную матрицы

--- Kashnikov_team_hw1/kun_by_vasileva.cpp
+++ Kashnikov_team_hw1/kun_by_vasileva.cpp
@@ -1,4 +1,71 @@
+#include "kun_by_vasileva.h"
+
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-#include "kun_by_vasileva.h"
+using namespace std;
+
+const int MAXN = 1000;
+
+vector<int> g[MAXN];
+int mt[MAXN];
+bool used[MAXN];
+
+bool try_kuhn(int v) {
+    if (used[v]) {
+        return false;
+    }
+    used[v] = true;
+    for (int i = 0; i < g[v].size(); i++) {
+        int to = g[v][i];
+        if (mt[to] == -1 || try_kuhn(mt[to])) {
+            mt[to] = v;
+            return true;
+        }
+    }
+    return false;
+}
+
+int kuhn(int n) {
+    fill(mt, mt + n, -1);
+    for (int v = 0; v < n; v++) {
+        fill(used, used + n, false);
+        try_kuhn(v);
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (mt[i] != -1) {
+            ans++;
+        }
+    }
+
+    return ans / 2;
+}
+
+int main() {
+    int n;
+    int m;
+    cout << "Vvedite kol-vo reber sprava: ";
+    cin >> n;
+    cout << "Vvedite kol-vo vershin sleva: ";
+    cin >> m;
+
+    vector<vector<int>> adj_matrix(n + m, vector<int>(n + m));
+
+    for (int i = 0; i < n + m; i++) {
+        for (int j = 0; j < n + m; j++) {
+            cout << "Vvedite " << i << j << " element matrix: ";
+            cin >> adj_matrix[i][j];
+            if (adj_matrix[i][j] == 1) {
+                g[i].push_back(j);
+            }
+        }
+    }
+
+    int max_matching = kuhn(n + m);
+    cout << "Max matching: " << max_matching << endl;
+
+    return 0;
+}
