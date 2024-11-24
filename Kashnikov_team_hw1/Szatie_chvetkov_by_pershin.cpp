167405b9cd67902d1f238bb14332b8be721e1674
pershinegor <egorpershin1@mail.ru>
2024-11-24 20:20:41 +0300

    цветки

---
+++ Kashnikov_team_hw1/Szatie_chvetkov_by_pershin.cpp
@@ -0,0 +1,83 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void delete_incsid(vector<vector<int>>& mas_vector, const vector<int>& rebro);
+void opr_vector(const vector<vector<int>>& mas, vector<vector<int>>& mas_vector);
+void opr_edinich(vector<vector<int>>& mas_vector, int& edinich);
+void postroenie_parosoch(vector<vector<int>>& mas_vector, vector<vector<int>>& parosoch);
+
+//vector<vector<int>> m = { массив над которым работал
+//	{0,0,0,1,1,0},{0,0,0,0,1,0},{0,0,0,0,0,1},{1,0,0,0,0,0},{1,1,0,0,0,0},{0,0,1,0,0,0} 
+//};
+
+int Szatie_chvetkov_by_pershin(const vector<vector<int>>& m) {
+	vector<vector<int>> mas_vector; // количество ребер в массиве и какие они
+	opr_vector(m, mas_vector);
+	int edinich = 0;
+	opr_edinich(mas_vector, edinich); // определение "единичных" ребер , которые не будут учавствовать 
+
+	vector<int> start = mas_vector[0]; // начало цепи
+	vector <vector<int>> parosoch;
+	parosoch.push_back(start);
+	mas_vector.erase(mas_vector.begin());
+	//delete_incsid(mas_vector, parosoch[0]);		< -- (поврежденная часть кода)
+	//postroenie_parosoch(mas_vector, parosoch);
+	//cout << start[0] << start[1] << endl;
+
+
+	int end = size(parosoch);
+
+	/*for (vector<int> m : mas_vector) {
+		for (int i : m) {
+			cout << i << endl;
+		}
+	}*/
+	return edinich + end;
+}
+
+void opr_vector(const vector<vector<int>>& m, vector<vector<int>>& mas_vector) {
+	for (int i = 0; i < size(m); i++) { // определение размера массива
+		for (int j = 0; j < size(m[0]); j++) {
+			if (i < j) {
+				if (m[i][j] == 1) {
+					mas_vector.push_back({ i + 1, j + 1 });
+				}
+			}
+		}
+	}
+}
+
+void opr_edinich(vector<vector<int>>& mas_vector, int& edinich) {
+	int diapozon = 0;
+	for (vector<int> m1 : mas_vector) {
+		int counter = 0;
+		for (vector<int> m : mas_vector) {
+			if (m1[0] == m[0] && m1[1] == m[1]) continue;
+			if (m1[0] == m[0] || m1[1] == m[1]) counter++;
+		}
+		if (counter == 0) {
+			edinich++;
+			mas_vector.erase(mas_vector.begin() + diapozon);
+		}
+		diapozon++;
+	}
+}
+
+void postroenie_parosoch(vector<vector<int>>& mas_vector, vector<vector<int>>& parosoch) {
+	for (vector<int> mas : mas_vector) {
+		parosoch.push_back(mas);
+		delete_incsid(mas_vector, mas);
+	}
+}
+
+void delete_incsid(vector<vector<int>>& mas_vector, const vector<int>& rebro) {
+	int counter = 0;
+	for (vector<int> mas : mas_vector) {
+		if (mas[0] == rebro[0] || mas[1] == rebro[1]) {
+			mas_vector.erase(mas_vector.begin() + counter);
+		}
+		counter++;
+	}
+}
