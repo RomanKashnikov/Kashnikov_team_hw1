273050ac6b66a73072fb1d139d2f205d615f355c
Ord1nI <ordinandrej@gmail.com>
2024-11-25 22:50:52 +0300

    add graphviz support

--- Kashnikov_team_hw1/graph_gen_by_ordin.cpp
+++ Kashnikov_team_hw1/graph_gen_by_ordin.cpp
@@ -1,12 +1,12 @@
 #include "graph_gen_by_ordin.h"
-#include <iostream>
 #include <vector>
+#include <fstream>
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
-vector<vector<int> > generateBipartiteGraphMatrix(const int& n1, const int& n2) {
+vector<vector<int> > generateBipartiteGraphMatrix(const int n1, const int n2) {
     vector<vector<int> > adjacencyMatrix(n1 + n2, vector<int>(n1 + n2, 0));
     srand(time(0));
 
@@ -22,4 +22,20 @@ vector<vector<int> > generateBipartiteGraphMatrix(const int& n1, const int& n2) 
     }
 
     return adjacencyMatrix;
+}
+
+
+void generateGraphvizFile(std::vector<std::vector<int> >& matrix, string fileName) {
+    ofstream file(fileName);
+    file << "graph G {\n";
+    for(size_t i = 0; i < matrix.size(); i++) {
+        for(size_t q = i; q < matrix[i].size(); q++) {
+            if (matrix[i][q] != 0) {
+                file << (char)'a'+i << "--" << (char)'a'+q << '\n'; 
+            }
+        }
+    }
+    file << '}';
+    file.close();
+
 }
--- Kashnikov_team_hw1/graph_gen_by_ordin.h
+++ Kashnikov_team_hw1/graph_gen_by_ordin.h
@@ -2,9 +2,12 @@
 #define _GRAPH_GEN_BY_ORDIN_
 
 #include <vector>
+#include <string>
 
 #define PROBABILITY 0.1
 
-std::vector<std::vector<int> > generateBipartiteGraphMatrix(const int& n1, const int& n2);
+std::vector<std::vector<int> > generateBipartiteGraphMatrix(const int n1, const int n2);
+
+void generateGraphvizFile(std::vector<std::vector<int> >& matrix, std::string fileName);
 
 #endif // _GRAPH_GEN_BY_ORDIN_
